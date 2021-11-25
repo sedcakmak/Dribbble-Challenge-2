04b573d29a8a33e572c51209c23caf8041282682
Sedef Cakmak <cakmaksedef@gmail.com>
2021-11-25 16:47:06 +0300

    continue styling

--- javascript/index.js
+++ javascript/index.js
@@ -33,15 +33,33 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
 function changeOutput() {
   document.getElementById('mail').value = "jeetseans@gma";
+  // document.getElementById('email').setAttribute('class', 'active');
+}
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("full-input");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
 }
